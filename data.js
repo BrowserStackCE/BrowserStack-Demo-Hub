746c441df211ba3c4f94329054b63be05e51a1cb
YashDalwani <yashdalwani21@gmail.com>
2026-07-27 14:01:40 +0530

    Added Test Companion

--- data.js
+++ data.js
@@ -653,14 +653,14 @@ id: "app-automate",
   },
 
 
-{
+  {
     id: "test-companion",
     name: "Test Companion",
     tagline: "Agentic AI for test automation",
     icon: "",
     iconSvg: "icons/icon-22.svg",
     videos: [
-      { id: "testcompanion-1", title: "Introducing Test Companion", youtubeId: "", duration: "04:29",
+      { id: "testcompanion-1", title: "Introducing Test Companion", youtubeId: "https://www.youtube.com/watch?v=ZckJuvKn2iE", duration: "06:11",
         description: "Test Companion helps SDETs author, execute, debug, and maintain tests 4X faster with agentic AI right inside the IDE they already use. Works with any automation framework to test mobile and web applications.",
         docs: [{label: "Test Companion Docs", url: "https://www.browserstack.com/docs/test-companion/overview"}], links: [{label: "Download the extention here", url:"https://www.browserstack.com/docs/test-companion/get-started/installation-guide"}] },
     ],
